8ced2562c6c60b0ec870e5077e959eed19a2d1fc
andresatierf <andrecfreitas58gmail.com>
2021-10-18 18:53:16 +0000

    Create available colors class

---
+++ src/org/academiadecodigo/bootcamp65/JavaColors.java
@@ -0,0 +1,31 @@
+package org.academiadecodigo.bootcamp65;
+
+
+import java.awt.*;
+
+public class JavaColors {
+    public static final Color[] colors = {
+            Color.YELLOW,
+            Color.CYAN,
+            Color.BLACK,
+            Color.BLUE,
+            Color.DARK_GRAY,
+            Color.GRAY,
+            Color.GREEN,
+            Color.LIGHT_GRAY,
+            Color.MAGENTA,
+            Color.ORANGE,
+            Color.PINK,
+            Color.RED,
+            Color.WHITE
+    };
+
+    public static int getColorIndex(Color color) {
+        for (int i = 0; i < colors.length; i++) {
+            if (colors[i] == color) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
